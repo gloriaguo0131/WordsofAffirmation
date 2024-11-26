fa249b203745d5903ffed4e93977fc0942ba1d22
gloriaguo0131 <gloriaguo0131>
2024-11-26 14:41:26 +0800

    update mobile

--- script.js
+++ script.js
@@ -9,6 +9,9 @@ const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
 
 // Update the background video source for mobile devices
 if (isMobile) {
+    const videoElement = document.getElementById('videoElement');
+    videoElement.style.width = '60vw'; 
+    videoElement.style.height = '30vh';
     backgroundSource.src = "mobile.mp4";
     backgroundVideo.load(); // Reload the video with the new source
 }
